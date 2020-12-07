e9da6c306092b5984d6adbef33965305d64801e1
Robert Wagner <rwwagner90@gmail.com>
2020-12-07 10:59:33 -0500

    Support dates

--- addon/utils/sort-by.ts
+++ addon/utils/sort-by.ts
@@ -2,11 +2,19 @@ interface IObjectKeys {
   [key: string]: any;
 }
 
-export default function sortBy(
-  array: Array<Object>,
-  key: string
-) {
+export default function sortBy(array: Array<Object>, key: string) {
   return array?.sort((item1: IObjectKeys, item2: IObjectKeys) => {
+    let item1Value = item1[key];
+    let item2Value = item2[key];
+
+    if (item1Value instanceof Date) {
+      item1Value = item1Value.toISOString();
+    }
+
+    if (item2Value instanceof Date) {
+      item2Value = item2Value.toISOString();
+    }
+
     if (item1[key] < item2[key]) {
       return -1;
     }
@@ -14,7 +22,7 @@ export default function sortBy(
     if (item1[key] < item2[key]) {
       return 1;
     }
-  
+
     return 0;
   });
 }
